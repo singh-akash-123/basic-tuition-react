b0cc553a613513433b4a7d37605ff7438f4e0b6b
Akash singh <robinakash123@gmail.com>
2020-05-07 02:36:16 +0530

    Updates

---
+++ precache-manifest.e2d569f9073b12e7e1ba959e39a38414.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4744da81ba7a698282da86b7298fef1e",
+    "url": "/basic-tuition-react/home/index.html"
+  },
+  {
+    "revision": "5a3dfcb02704f1df716c",
+    "url": "/basic-tuition-react/home/static/css/main.66769509.chunk.css"
+  },
+  {
+    "revision": "bc33604b968e30d965d3",
+    "url": "/basic-tuition-react/home/static/js/2.c13130da.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/basic-tuition-react/home/static/js/2.c13130da.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5a3dfcb02704f1df716c",
+    "url": "/basic-tuition-react/home/static/js/main.5f76b8e0.chunk.js"
+  },
+  {
+    "revision": "f406ec9ac703e77f5f7d",
+    "url": "/basic-tuition-react/home/static/js/runtime-main.01554d3d.js"
+  }
+]);
