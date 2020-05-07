5be266f7055268a67524dd41a6d2be4c3224e663
Akash singh <robinakash123@gmail.com>
2020-05-07 12:04:32 +0530

    Updates

---
+++ precache-manifest.da6c688624ce8873dc74a1113a411326.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28f46950d276768c4adb95e925a46cfb",
+    "url": "/basic-tuition-react/index.html"
+  },
+  {
+    "revision": "b5105c7a2452b3a521fb",
+    "url": "/basic-tuition-react/static/css/main.66769509.chunk.css"
+  },
+  {
+    "revision": "daffcd26981f24ff66e3",
+    "url": "/basic-tuition-react/static/js/2.8dd4d6ab.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/basic-tuition-react/static/js/2.8dd4d6ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b5105c7a2452b3a521fb",
+    "url": "/basic-tuition-react/static/js/main.76c6eca6.chunk.js"
+  },
+  {
+    "revision": "d8f761e960264431c3c2",
+    "url": "/basic-tuition-react/static/js/runtime-main.0fce04e7.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/basic-tuition-react/home/precache-manifest.e2d569f9073b12e7e1ba959e39a38414.js"
+  "/basic-tuition-react/precache-manifest.da6c688624ce8873dc74a1113a411326.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/basic-tuition-react/home/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/basic-tuition-react/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
